cc6bfb1117eb021af5c1dae26071a832799e193d
xLuxuriax <mr.darksergei@gmail.com>
2024-09-29 13:13:00 +0300

    Домашняя работа

---
+++ src/main/java/Car.java
@@ -0,0 +1,17 @@
+public class Car {
+    private String name;
+    private int speed;
+
+    public Car(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+}
