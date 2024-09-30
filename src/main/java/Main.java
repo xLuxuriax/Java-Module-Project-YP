2ca3c3700e82c92fa492b20f5eb65c2988ea2253
xLuxuriax <mr.darksergei@gmail.com>
2024-09-30 19:21:02 +0300

    Исправлен недочет связанный с ведением неправильным введением чисел.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,16 +12,22 @@ public class Main {
 
             int speed;
             while (true) {
-                System.out.print("Ввведите скорость (0-250): ");
-                speed = scanner.nextInt();
-                scanner.nextLine();
+                try {
+                    System.out.print("Ввведите скорость (0-250): ");
+                    speed = scanner.nextInt();
+                    scanner.nextLine();
 
-                if (speed >= 0 && speed <= 250) {
-                    break;
-                } else {
-                    System.out.println("Неверная скорость, введите заново.");
+                    if (speed >= 0 && speed <= 250) {
+                        break;
+                    } else {
+                        System.out.println("Неверная скорость, введите заново.");
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Не правильный ввод. Тебе же сказали вводить цифры.");
+                    scanner.next();
                 }
             }
+
 
             Car car = new Car(name, speed);
             game.setLeader(car);
